4b2b788e8d7bee7dd066ae6f4b38a96412ebb4cc
P <p7ll7p@gmail.com>
2020-03-22 22:33:36 +0400

    almost done viewingmode

--- Assets/Scripts/DBManager.cs
+++ Assets/Scripts/DBManager.cs
@@ -5,31 +5,47 @@ using Mono.Data.Sqlite;
 using System.Data;
 using System;
 using System.IO;
+using UnityEngine.Networking;
 
+public struct pairOfWord
+{
+    public string word;
+    public string translate;
+}
 public class DBManager : MonoBehaviour
 {
+    List<pairOfWord> words= new List<pairOfWord>();
+    //Dictionary<string, string> words = new Dictionary<string, string>();
     private SqliteConnection con2db;
     private string path2db; //путь
-    public string dbName;
+    private string dbName = "db.s3db";
     private SqliteCommand cmd2db;
-    private SqliteDataReader dbReader;
+    public SqliteDataReader dbReader;
 
     private static DBManager instance;
 
-    private DBManager()
-    {
 
+
+    [SerializeField] string command = "SELECT * FROM words;";
+
+    private void Awake()
+    {
+        connect();
     }
+
 
     public static DBManager getInstance()
     {
         if (!instance)
-            instance = new DBManager();
+        {
+            GameObject dbManager = new GameObject("dbManager");
+            instance = dbManager.AddComponent<DBManager>();
+        }
         return instance;
     }
 
 
-    private void connect()
+    public void connect()
     {
         try
         {
@@ -38,6 +54,7 @@ public class DBManager : MonoBehaviour
                 path2db = Application.persistentDataPath + "/" + dbName;
                 if (!File.Exists(path2db))
                 {
+                    //UnityWebRequest loadDB = UnityWebRequest.
                     WWW loadDB = new WWW("jar:file://" + Application.dataPath + "!/assets/" + dbName);
                     while (!loadDB.isDone) { }
                     File.WriteAllBytes(path2db, loadDB.bytes);
@@ -45,29 +62,110 @@ public class DBManager : MonoBehaviour
             }
             else
                 path2db = Application.dataPath + "/" + dbName;
+
             con2db = new SqliteConnection("URI=file:" + path2db);
             con2db.Open();
-            //if (con2db.State.Equals("open"))// == connectionstate.open
-            //{
-
-            //}
+            if (con2db.State == ConnectionState.Open)
+            {
+                Debug.Log("connected!!");
+            }
+            else
+            {
+                Debug.Log("pisos");
+            }
         }
         catch(Exception exeption)
         {
             Debug.Log(exeption.ToString());
         }
+      
+    }
+    public void disconnect()
+    {
+        try
+        {
+            con2db.Close();
+        }
+        catch (Exception ex) {
+            Debug.Log(ex.ToString());
+        }
+    }
 
+    bool insertData2DB(string word, string translate)
+    {
+        string com4existingNewWord = "SELECT * FROM words WHERE word = '" + word +
+            "'and translate = '" + translate + "';";
+        cmd2db = new SqliteCommand(com4existingNewWord, con2db);
+
+        if (cmd2db.ExecuteNonQuery() != 0)
+        {
+            try
+            {
+                string insCom = "INSERT INTO words (word, translate) " +
+                        "VALUES ('" + word + "', '" + translate + "');";
+                cmd2db = new SqliteCommand(insCom, con2db);
+                cmd2db.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(ex.ToString());
+            }
+            return true;
+        }
+        else
+        {
+            Debug.Log("yet exist");
+            return false;
+        }
+       
+    }
+
+    
+    public void executeData(List<pairOfWord> words )
+    {
+        
+        try
+        {
+            cmd2db = new SqliteCommand(command, con2db);
+            dbReader = cmd2db.ExecuteReader();
+
+            string word, translate;
+            pairOfWord tempPairOfWord;
+            while (dbReader.Read())
+            {
+                word = dbReader[1].ToString();
+                translate = dbReader[2].ToString();
+                tempPairOfWord.word = word;
+                tempPairOfWord.translate = translate;
+                if(!words.Contains(tempPairOfWord))
+                    words.Add(tempPairOfWord);
+
+                //words.Values()
+                //if(words.)
+                //words.Add(word, translate);
+
+                Debug.Log("get in loop");
+
+                Debug.Log(word);
+                Debug.Log(translate);
+               
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+       
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        connect();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    private void OnDestroy()
+    {
+        disconnect();
+    }
+  
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    List<pairOfWord> words = new List<pairOfWord>();
+    DBManager dbMan;
+    static GameManager getInstance()
+    {
+        if (!instance)
+            instance = new GameManager();
+        return instance;
+    }
+
+
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        dbMan = DBManager.getInstance();
+        // dbMan = DBManager.getInstance();
+        if (!dbMan)
+            Debug.Log("pizzzzzdec");
+        else
+        {
+            dbMan.connect();
+            dbMan.executeData(words);
+        }
+
+        int a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
